dbd9d72e135e42c7339ac487fb8516a1575e31c6
liucas00 <vominhthu3108@gmail.com>
2025-10-28 02:35:11 +0700

    Update UserProfile.tsx

--- apps/web/src/pages/UserProfile.tsx
+++ apps/web/src/pages/UserProfile.tsx
@@ -23,13 +23,16 @@ const ColMenu = styled.div`
     flex-basis: 300px; // Chiều rộng cố định
     flex-shrink: 0; // Không co lại
     background-color: #fff;
-    box-shadow: 0 2px 8px rgba(0,0,0,0.05);
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.05);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
 const HeaderMenu = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 50px;
+    margin-top: 30px;
 `
 const UserAvatar = styled(FaUserCircle)`
     font-size: 80px;
@@ -45,10 +48,15 @@ const UserName = styled.h3`
 const NavTitle = styled.h3`
     display: flex;
     justify-content: center;
+    font-weight: normal;
+    margin: 0;
+    color: #8c8c99;
 `
 const NavMenu = styled.nav`
     display:flex;
     flex-direction:column;
+    border-bottom: 1px solid #eee;
+
 `
 const MenuLink = styled(NavLink)`
     display: flex;
@@ -92,8 +100,7 @@ const MenuLink = styled(NavLink)`
 const LogoutContainer = styled.div`
     display: flex;
     justify-content: center;    
-    padding: 50px;
-    border-top: 1px solid #eee;
+    margin: 50px;
     background: #fcfcfc;
 `
 //cột Main
@@ -101,7 +108,7 @@ const ColMain = styled.div`
     flex-grow: 1; // Lấp đầy không gian còn lại
     background-color: #fff;
     padding: 30px 40px;
-    box-shadow: 0 2px 8px rgba(0,0,0,0.05);
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.05);
 `
 const MainTitle = styled.h1`
     display: flex;
@@ -110,27 +117,98 @@ const MainTitle = styled.h1`
     color: white;
     padding: 10px;
 `
-const MainText = styled.p``
+
 const AvaWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 50px auto;
+    margin: 40px auto;
 `
-const InfoWrapper = styled.div`
+const MessContainer = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin-bottom: 30px;
+`
+const MessTitle = styled.h2`
+    margin: 0;
+`
+const MessText = styled.p`
+    margin:0;
+`
+
+const BoxInfo = styled.div`
+    display: flex;
+    justify-content: space-between;
     border: 2px solid silver;
     border-radius: 10px;
-    padding-left: 20px;
+    padding: 20px;
+    margin: 50px 30px;
 `
-const InfoGroup = styled.div`
+const InfoMem = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
+const StatusMemGroup = styled.div`
     display: flex;
     align-items: center;
-    gap: 20px;
+    gap: 15px;
 `
-const InfoTitle = styled.h4``
-const InfoText = styled.p``
+const StatusMemTitle = styled.h4`
+    margin: 0;
+`
+const StatusMemText = styled.p`
+    margin: 0;
+`
+const CardGroup = styled.div`
+    display: flex;
+    
+`
+const GiftCardGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    border-left: 1px solid #222;
+    padding: 0 20px;
+`
+const GiftCardTitle = styled.h4`
+    margin: 0;
+`
+const GiftCardText = styled.p`
+    margin: 0;
+`
+const VoucherGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    border-left: 1px solid #222;
+    padding: 0 20px;
+`
+const VoucherTitle = styled.h4`
+    margin: 0;
+`
+const VoucherText = styled.p`
+    margin: 0;
+`
+const CouponGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    border-left: 1px solid #222;
+    padding: 0 20px;
+`
+const CouponTitle = styled.h4`
+    margin: 0;
+`
+const CouponText = styled.p`
+    margin: 0;
+`
 
 
 export default function UserProfile () {
@@ -146,8 +224,9 @@ export default function UserProfile () {
                 <HeaderMenu>
                     <UserAvatar />
                     <UserName>{displayUser.name}</UserName>
+                    <NavTitle>Member</NavTitle>
                 </HeaderMenu>
-                <NavTitle>Member</NavTitle>
+                
                 <NavMenu>
                     <MenuLink to="/userProfile" end>
                         <FaThLarge /> Thông tin chung
@@ -176,24 +255,47 @@ export default function UserProfile () {
                 <UserAvatar />
                     <Button $padding='5px 10px' $background='#9d9b9b'>Thay đổi</Button>
                 </AvaWrapper>
-                <MainText>
-                    Xin chào Võ Minh Thư,
-                    <br/> Với trang này, bạn sẽ quản lý được tất cả thông tin tài khoản của mình.
-                </MainText>   
-                <InfoWrapper>
-                    <InfoGroup>
-                        <InfoTitle>Hạng thành viên:</InfoTitle>
-                        <InfoText>Member</InfoText>
-                    </InfoGroup>
-                    <InfoGroup>
-                        <InfoTitle>Tổng chi tiêu:</InfoTitle>
-                        <InfoText>0</InfoText>
-                    </InfoGroup>
-                    <InfoGroup>
-                        <InfoTitle>Điểm thưởng:</InfoTitle>
-                        <InfoText>0</InfoText>
-                    </InfoGroup>
-                </InfoWrapper> 
+                <MessContainer>
+                    <MessTitle>
+                        Xin chào Võ Minh Thư,
+                    </MessTitle> 
+                    <MessText>
+                        Với trang này, bạn sẽ quản lý được tất cả thông tin tài khoản của mình.
+                    </MessText>
+                </MessContainer>  
+                <BoxInfo>
+                    <InfoMem>
+                    <StatusMemGroup>
+                        <StatusMemTitle>Hạng thành viên:</StatusMemTitle>
+                        <StatusMemText>Member</StatusMemText>
+                    </StatusMemGroup>
+                    <StatusMemGroup>
+                        <StatusMemTitle>Tổng chi tiêu:</StatusMemTitle>
+                        <StatusMemText>0</StatusMemText>
+                    </StatusMemGroup>
+                    <StatusMemGroup>
+                        <StatusMemTitle>Điểm thưởng:</StatusMemTitle>
+                        <StatusMemText>0</StatusMemText>
+                    </StatusMemGroup>
+                    </InfoMem>
+                    <CardGroup>
+                        <GiftCardGroup>
+                            <GiftCardTitle>Thẻ quà tặng</GiftCardTitle>
+                            <GiftCardText>0</GiftCardText>
+                            <Button $padding='3px 20px'>Xem</Button>
+                        </GiftCardGroup>
+                        <VoucherGroup>
+                            <VoucherTitle>Voucher</VoucherTitle>
+                            <VoucherText>0</VoucherText>
+                            <Button $padding='3px 20px'>Xem</Button>
+                        </VoucherGroup>
+                        <CouponGroup>
+                            <CouponTitle>Coupon</CouponTitle>
+                            <CouponText>0</CouponText>
+                            <Button $padding='3px 20px'>Xem</Button>
+                        </CouponGroup>
+                    </CardGroup>
+                </BoxInfo> 
             </ColMain>
         </MainContainer>
 
